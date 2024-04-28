35cd032ee18aacd09b8fcd65dd6a36118b3df35f
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-04-28 17:16:55 +0600

    handle error that can not handle try catch

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,9 +8,25 @@ import config from "./config"
 async function main() {
     const port = config.port
     const server: Server = app.listen(port, () => {
-        console.log(`Health care server is running on  ${port} port`)
+        console.log(`Health care server is running on  ${port}`)
     })
+    const exitHandler = () => {
+        if (server) {
+            server.close(() => {
+                console.info("Server closed!")
+            })
+        }
+        process.exit(1);
+    };
+    process.on('uncaughtException', (error) => {
+        console.log(error);
+        exitHandler();
+    });
 
+    process.on('unhandledRejection', (error) => {
+        console.log(error);
+        exitHandler();
+    })
 }
 
 main()
