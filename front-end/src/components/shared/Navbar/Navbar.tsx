afa8b4afdb29aa538ae2a105e17aa6877293917e
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-05-05 22:53:32 +0600

    add all images and navbar and footer

--- front-end/src/components/shared/Navbar/Navbar.tsx
+++ front-end/src/components/shared/Navbar/Navbar.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>this si navbar page </h1>
-    </div>
+    <Container>
+      <Stack
+        py={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography variant="h4" component={Link} href="/" fontWeight={600}>
+          P
+          <Box component="span" color="primary.main">
+            H
+          </Box>{" "}
+          Health Care
+        </Typography>
+
+        <Stack direction="row" justifyContent="space-between" gap={4}>
+          <Typography component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography>Health Plans</Typography>
+          <Typography>Medicine</Typography>
+          <Typography>Diagnostics</Typography>
+          <Typography>NGOs</Typography>
+        </Stack>
+
+        <Button component={Link} href="/login">
+          Login
+        </Button>
+      </Stack>
+    </Container>
   );
 };
 
