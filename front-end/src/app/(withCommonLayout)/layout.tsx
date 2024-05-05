65c36ff1081684f994504ab4cef98b84fb114a07
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-05-05 22:36:14 +0600

    basic layout set up

---
+++ front-end/src/app/(withCommonLayout)/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/shared/Footer/Footer";
+import Navbar from "@/components/shared/Navbar/Navbar";
+
+const CommonLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>this si our main layout</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default CommonLayout;
