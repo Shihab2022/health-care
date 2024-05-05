9e0c02b508caf879b338532e03b575c2c78c060a
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-05-05 21:53:30 +0600

    basic next app setup

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+const HomePage = () => {
+  return (
+    <>
+      <Stack>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </>
+  );
+};
+
+export default HomePage;
