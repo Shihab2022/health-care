b1edf954682e48f8ff3472795baeb7a79d10db82
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-05-10 22:37:09 +0600

    create top rated section

--- front-end/src/app/(withCommonLayout)/page.tsx
+++ front-end/src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/ui/HomePage/HeroSection/HeroSection";
 import Specialist from "@/components/ui/HomePage/Specialist/Specialist";
+import TopRatedDoctors from "@/components/ui/HomePage/TopRatedDoctors/TopRatedDoctors";
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <Specialist />
+      <TopRatedDoctors />
     </>
   );
 };
