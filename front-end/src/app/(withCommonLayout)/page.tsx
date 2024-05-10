28b1879fb9c83074d9ccbb6a41d21c87869412ff
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-05-10 22:38:46 +0600

    create why us section

--- front-end/src/app/(withCommonLayout)/page.tsx
+++ front-end/src/app/(withCommonLayout)/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/ui/HomePage/HeroSection/HeroSection";
 import Specialist from "@/components/ui/HomePage/Specialist/Specialist";
 import TopRatedDoctors from "@/components/ui/HomePage/TopRatedDoctors/TopRatedDoctors";
+import WhyUs from "@/components/ui/HomePage/WhyUs/WhyUs";
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <Specialist />
       <TopRatedDoctors />
+      <WhyUs />
     </>
   );
 };
