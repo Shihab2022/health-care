2c6dbc31f5278d15c925f477d2ce2e44ec42f660
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-05-10 22:52:33 +0600

    completed home page ui

--- front-end/src/app/(withCommonLayout)/page.tsx
+++ front-end/src/app/(withCommonLayout)/page.tsx
@@ -1,5 +1,7 @@
 import HeroSection from "@/components/ui/HomePage/HeroSection/HeroSection";
+import HowItWorks from "@/components/ui/HomePage/HowItWorks/HowItWorks";
 import Specialist from "@/components/ui/HomePage/Specialist/Specialist";
+import Stats from "@/components/ui/HomePage/Stats/Stats";
 import TopRatedDoctors from "@/components/ui/HomePage/TopRatedDoctors/TopRatedDoctors";
 import WhyUs from "@/components/ui/HomePage/WhyUs/WhyUs";
 const HomePage = () => {
@@ -9,6 +11,8 @@ const HomePage = () => {
       <Specialist />
       <TopRatedDoctors />
       <WhyUs />
+      <HowItWorks />
+      <Stats />
     </>
   );
 };
