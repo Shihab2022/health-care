01c8eb0fdd1b49fd4b3a824529d22d1ab65dac13
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-05-11 22:39:07 +0600

    implement login and register

--- front-end/src/components/shared/Navbar/Navbar.tsx
+++ front-end/src/components/shared/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
+"use client";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import Link from "next/link";
+import dynamic from "next/dynamic";
 
 const Navbar = () => {
+  const AuthButton = dynamic(
+    () => import("@/components/ui/AuthButton/AuthButton"),
+    { ssr: false }
+  );
   return (
     <Container>
       <Stack
@@ -28,9 +34,7 @@ const Navbar = () => {
           <Typography>NGOs</Typography>
         </Stack>
 
-        <Button component={Link} href="/login">
-          Login
-        </Button>
+        <AuthButton />
       </Stack>
     </Container>
   );
