738ec4dcf4c2d952973c7a423417431e799fd1db
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-05-19 22:23:51 +0600

    fix dialog issue

--- front-end/src/components/shared/PHModal/PHFullScreenModal.tsx
+++ front-end/src/components/shared/PHModal/PHFullScreenModal.tsx
@@ -4,8 +4,7 @@ import CloseIcon from "@mui/icons-material/Close";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
 import { DialogContent, DialogTitle, SxProps } from "@mui/material";
-import { BootstrapDialog } from "./PHModal";
-
+import { BootstrapDialog } from "./Dialog";
 type TModalProps = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
