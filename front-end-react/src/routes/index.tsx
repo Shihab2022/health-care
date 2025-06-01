30289aab8784b483c68e303326bfff3e53ff21bc
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-06-01 17:15:06 +0600

    add not  fount page

--- front-end-react/src/routes/index.tsx
+++ front-end-react/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Navigate, useRoutes } from "react-router-dom";
 import LoginPage from "../pages/login";
 import HomePageOne from "../pages/home";
 import Register from "../pages/register";
+import Page404 from "../pages/404";
 export default function Router() {
   return useRoutes([
     // {
@@ -334,6 +335,6 @@ export default function Router() {
     {  path: "/", element:  <HomePageOne /> },
     {  path: "/login", element: <LoginPage /> },
     {  path: "/register", element: <Register /> },
-    { path: "*", element: <Navigate to="/404" replace /> },
+    { path: "*", element: <Page404 /> },
   ]);
 }
