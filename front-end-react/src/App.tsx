fe5c979b6ff684188ea43574f2b093407b13d0d4
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-06-01 11:47:11 +0600

    add provider

--- front-end-react/src/App.tsx
+++ front-end-react/src/App.tsx
@@ -1,19 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.css';
-import HomePageOne from './pages/home';
-import 'swiper/css';
+import { Provider } from "react-redux";
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Router from './routes';
 
+import { store } from './store';
+
 const router = createBrowserRouter([{ path: "*", Component: Router }]);
 function App() {
   return (
     <>
-        {/* <Provider store={store}></Provider> */}
+        <Provider store={store}>
              <RouterProvider router={router} />
-     
+     </Provider>
     </>
   );
 }
