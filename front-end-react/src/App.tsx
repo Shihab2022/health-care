c789cf12139aca81dc9a17b689130bda2a958762
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-06-01 09:58:53 +0600

    set up route

--- front-end-react/src/App.tsx
+++ front-end-react/src/App.tsx
@@ -1,12 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.css';
 import HomePageOne from './pages/home';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Router from './routes';
+
+const router = createBrowserRouter([{ path: "*", Component: Router }]);
 function App() {
   return (
     <>
-      <HomePageOne />
+        {/* <Provider store={store}></Provider> */}
+             <RouterProvider router={router} />
+     
     </>
   );
 }
