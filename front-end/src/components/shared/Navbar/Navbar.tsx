d59542c2c79aad7e004eef9ed106f9ab9dda2737
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-07-12 12:05:51 +0600

    add link on home page route

--- front-end/src/components/shared/Navbar/Navbar.tsx
+++ front-end/src/components/shared/Navbar/Navbar.tsx
@@ -3,12 +3,15 @@ import { Container, Stack, Typography } from "@mui/material";
 import Link from "next/link";
 import dynamic from "next/dynamic";
 import Logo from "../Logo/Logo";
+import useUserInfo from "@/hooks/useUserInfo";
 
 const Navbar = () => {
+  const userInfo = useUserInfo();
   const AuthButton = dynamic(
     () => import("@/components/ui/AuthButton/AuthButton"),
     { ssr: false }
   );
+  console.log({ userInfo });
   return (
     <Container>
       <Stack
@@ -20,13 +23,20 @@ const Navbar = () => {
         <Logo isBgWhite={true} />
 
         <Stack direction="row" justifyContent="space-between" gap={4}>
-          <Typography component={Link} href="/consultation">
+          <Typography component={Link} href="/consultation" color="#000">
             Consultation
           </Typography>
-          <Typography>Health Plans</Typography>
-          <Typography>Medicine</Typography>
-          <Typography>Diagnostics</Typography>
-          <Typography>NGOs</Typography>
+
+          <Typography color="#000">Diagnostics</Typography>
+          <Typography component={Link} href="/doctors" color="#000">
+            Doctors
+          </Typography>
+
+          {userInfo?.email ? (
+            <Typography component={Link} href="/dashboard" color="#000">
+              Dashboard
+            </Typography>
+          ) : null}
         </Stack>
 
         <AuthButton />
