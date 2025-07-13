d06138cefa5e2d564e0c1b5cb2bffb62d68cc484
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-07-13 17:58:14 +0600

    showing all appointment in dashboard

--- front-end/src/app/(withDashboardLayout)/dashboard/patient/appointments/page.tsx
+++ front-end/src/app/(withDashboardLayout)/dashboard/patient/appointments/page.tsx
@@ -1,9 +1,101 @@
-const Appointments = () => {
+"use client";
+import { useGetMyAppointmentsQuery } from "@/redux/api/appointmentApi";
+import { Box, Chip, IconButton } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import Link from "next/link";
+import { dateFormatter } from "@/utils/dateFormatter";
+import { getTimeIn12HourFormat } from "../../doctor/schedules/components/MultipleSelectFieldChip";
+import PhChips from "@/components/shared/PhChip/PhChips";
+
+const PatientAppointmentsPage = () => {
+  const { data, isLoading } = useGetMyAppointmentsQuery({});
+  const appointments = data?.appointments?.data;
+  const meta = data?.appointments?.meta;
+  console.log({ data });
+  const columns: GridColDef[] = [
+    {
+      field: "name",
+      headerName: "Doctor Name",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return row.doctor.name;
+      },
+    },
+    {
+      field: "appointmentDate",
+      headerName: "Appointment Date",
+      headerAlign: "center",
+      align: "center",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return dateFormatter(row.schedule.startDateTime);
+      },
+    },
+    {
+      field: "appointmentTime",
+      headerName: "Appointment Time",
+      headerAlign: "center",
+      align: "center",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return getTimeIn12HourFormat(row.schedule.startDateTime);
+      },
+    },
+
+    {
+      field: "paymentStatus",
+      headerName: "Payment Status",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return row.paymentStatus === "PAID" ? (
+          <PhChips label={row.paymentStatus} type="success" />
+        ) : (
+          <PhChips label={row.paymentStatus} type="error" />
+        );
+      },
+    },
+    {
+      field: "action",
+      headerName: "Join",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return (
+          <IconButton
+            component={Link}
+            href={`/video?videoCallingId=${row?.videoCallingId}`}
+            disabled={row.paymentStatus === "UNPAID"}
+          >
+            <VideocamIcon
+              sx={{
+                color: row.paymentStatus === "PAID" ? "primary.main" : "",
+              }}
+            />
+          </IconButton>
+        );
+      },
+    },
+  ];
+
   return (
-    <div>
-      <h1>Appointments Dashboard</h1>
-    </div>
+    <Box>
+      {!isLoading ? (
+        <Box my={2}>
+          <DataGrid
+            rows={appointments ?? []}
+            columns={columns}
+            loading={isLoading}
+          />
+        </Box>
+      ) : (
+        <h1>Loading.....</h1>
+      )}
+    </Box>
   );
 };
 
-export default Appointments;
+export default PatientAppointmentsPage;
--- server/src/app/modules/Appointment/appointment.routes.ts
+++ server/src/app/modules/Appointment/appointment.routes.ts
@@ -1,34 +1,32 @@
-
-import express from 'express'
-import auth from '../../middlewares/auth';
-import { UserRole } from '@prisma/client';
-import { AppointmentController } from './appointment.controller';
+import express from "express";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+import { AppointmentController } from "./appointment.controller";
 
 const router = express.Router();
 
-
 router.get(
-    '/',
-    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
-    AppointmentController.getAllFromDB
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AppointmentController.getAllFromDB
 );
 
 router.get(
-    '/my-appointment',
-    auth(UserRole.PATIENT, UserRole.DOCTOR),
-    AppointmentController.getMyAppointment
-)
+  "/my-appointments",
+  auth(UserRole.PATIENT, UserRole.DOCTOR),
+  AppointmentController.getMyAppointment
+);
 
 router.post(
-    '/',
-    auth(UserRole.PATIENT),
-    // validateRequest(AppointmentValidation.createAppointment),
-    AppointmentController.createAppointment
+  "/",
+  auth(UserRole.PATIENT),
+  // validateRequest(AppointmentValidation.createAppointment),
+  AppointmentController.createAppointment
 );
 
 router.patch(
-    '/status/:id',
-    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
-    AppointmentController.changeAppointmentStatus
+  "/status/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+  AppointmentController.changeAppointmentStatus
 );
 export const AppointmentRoutes = router;
