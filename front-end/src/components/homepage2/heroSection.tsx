22efc8506c3b880270290cb332cb5de9267157fa
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-07-20 12:55:41 +0600

    add video iframe  for video

--- front-end/src/components/homepage2/heroSection.tsx
+++ front-end/src/components/homepage2/heroSection.tsx
@@ -1,9 +1,11 @@
+"use client";
+import { useState } from "react";
 import assets from "@/assets";
-import { Avatar, Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Modal, Stack, Typography } from "@mui/material";
 import Image from "next/image";
-import React from "react";
-
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
 const HeroSection2 = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <Box
@@ -53,7 +55,7 @@ const HeroSection2 = () => {
                 variant="h2"
                 sx={{
                   fontWeight: 700,
-                  fontSize: { xs: "2rem", md: "3.5rem" },
+                  fontSize: { xs: "2rem", md: "72px" },
                 }}
               >
                 Your Partner in <br /> Health and Wellness
@@ -61,7 +63,7 @@ const HeroSection2 = () => {
               <Typography
                 variant="body1"
                 sx={{
-                  mt: 3,
+                  my: 3,
                   color: "text.secondary",
                   fontSize: { xs: "1rem", md: "1.2rem" },
                 }}
@@ -69,19 +71,46 @@ const HeroSection2 = () => {
                 We are committed to providing you with the best medical and
                 healthcare services to help you live healthier and happier.
               </Typography>
-
-              <Button
-                variant="outlined"
+              <Box
                 sx={{
-                  mt: 4,
-                  borderRadius: 10,
-                  px: 3,
-                  py: 1.5,
-                  textTransform: "none",
+                  display: "flex",
+                  alignItems: "center",
+                  cursor: "pointer",
+                  marginTop: "40px",
+
+                  "&:hover .hover-text::after": {
+                    transform: "scaleX(1)",
+                  },
                 }}
               >
-                See how we work
-              </Button>
+                <PlayCircleOutlineIcon
+                  onClick={() => setOpen(true)}
+                  sx={{ fontSize: "60px" }}
+                />
+
+                <Typography
+                  className="hover-text"
+                  sx={{
+                    position: "relative",
+                    display: "inline-block",
+                    fontSize: "20px",
+                    "&::after": {
+                      content: '""',
+                      position: "absolute",
+                      left: 0,
+                      bottom: 0,
+                      height: "2px",
+                      width: "100%",
+                      backgroundColor: "#000",
+                      transform: "scaleX(0)",
+                      transformOrigin: "left",
+                      transition: "transform 0.8s ease",
+                    },
+                  }}
+                >
+                  See how we work
+                </Typography>
+              </Box>
             </Box>
           </Stack>
 
@@ -98,12 +127,42 @@ const HeroSection2 = () => {
             <Image
               src={assets?.layout.home1}
               alt="Doctors"
-              //   fill
               style={{ objectFit: "cover" }}
             />
           </Box>
         </Stack>
       </Box>
+
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: { xs: "90%", md: "1450px" },
+            height: { xs: "50vh", md: "750px" },
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            outline: "none",
+          }}
+        >
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/VcaAVWtP48A?autoplay=1"
+            title="YouTube video player"
+            style={{ border: "none" }}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </Box>
+      </Modal>
     </>
   );
 };
