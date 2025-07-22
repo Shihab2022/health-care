7d29ee868a0c9c9f26a6db46dedf702a3cdb5937
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-07-22 22:55:33 +0600

    create config folder for dame data

--- front-end/src/components/homepage2/valuesSection.tsx
+++ front-end/src/components/homepage2/valuesSection.tsx
@@ -11,44 +11,8 @@ import type { Swiper as SwiperClass } from "swiper/types";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { OurValuesConfig } from "@/config";
 
-const config = [
-  {
-    id: 2,
-    icon: assets.svgs.compassion,
-    title: "Compassion",
-    description:
-      "We understand that seeking medical care can be a stressful and emotional experience, and we strive to create a welcoming and supportive environment that puts our patients at ease and every one.",
-  },
-  {
-    id: 244,
-    icon: assets.svgs.excellence,
-    title: "Excellence",
-    description:
-      "We are committed to providing excellent medical care and services to our patients. We believe in continuously improving our skills, knowledge, and resources to ensure that we deliver the highest quality care possible.",
-  },
-  {
-    id: 2233,
-    icon: assets.svgs.integrity,
-    title: "Integrity",
-    description:
-      "We believe in practicing medicine with integrity and honesty. We are transparent in our communication and decision-making processes, and we always put our patient's interests first & provide best solution.",
-  },
-  {
-    id: 2322,
-    icon: assets.svgs.compassion,
-    title: "Respect",
-    description:
-      "We understand that seeking medical care can be a stressful and emotional experience, and we strive to create a welcoming and supportive environment that puts our patients at ease and every one.",
-  },
-  {
-    id: 2342,
-    icon: assets.svgs.excellence,
-    title: "Teamwork",
-    description:
-      "We understand that seeking medical care can be a stressful and emotional experience, and we strive to create a welcoming and supportive environment that puts our patients at ease and every one.",
-  },
-];
 const ValuesCard = ({ value }: any) => {
   const { icon, title, description } = value;
   return (
@@ -186,7 +150,7 @@ const ValuesSection = () => {
                 allowTouchMove={true}
                 style={{ overflow: "hidden" }}
               >
-                {config.map((c) => (
+                {OurValuesConfig.map((c) => (
                   <>
                     <SwiperSlide key={c?.id}>
                       <ValuesCard value={c} />
---
+++ front-end/src/config/index.ts
@@ -0,0 +1,39 @@
+import assets from "@/assets";
+
+export const OurValuesConfig = [
+  {
+    id: 2,
+    icon: assets.svgs.compassion,
+    title: "Compassion",
+    description:
+      "We understand that seeking medical care can be a stressful and emotional experience, and we strive to create a welcoming and supportive environment that puts our patients at ease and every one.",
+  },
+  {
+    id: 244,
+    icon: assets.svgs.excellence,
+    title: "Excellence",
+    description:
+      "We are committed to providing excellent medical care and services to our patients. We believe in continuously improving our skills, knowledge, and resources to ensure that we deliver the highest quality care possible.",
+  },
+  {
+    id: 2233,
+    icon: assets.svgs.integrity,
+    title: "Integrity",
+    description:
+      "We believe in practicing medicine with integrity and honesty. We are transparent in our communication and decision-making processes, and we always put our patient's interests first & provide best solution.",
+  },
+  {
+    id: 2322,
+    icon: assets.svgs.compassion,
+    title: "Respect",
+    description:
+      "We understand that seeking medical care can be a stressful and emotional experience, and we strive to create a welcoming and supportive environment that puts our patients at ease and every one.",
+  },
+  {
+    id: 2342,
+    icon: assets.svgs.excellence,
+    title: "Teamwork",
+    description:
+      "We understand that seeking medical care can be a stressful and emotional experience, and we strive to create a welcoming and supportive environment that puts our patients at ease and every one.",
+  },
+];
